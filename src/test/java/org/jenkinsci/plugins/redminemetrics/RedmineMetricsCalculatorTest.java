153d50cd62b203c701fba26dec0bbb3bcabe963f
Mitoma Ryo <mutetheradio@gmail.com>
2012-10-06 12:00:23 +0900

    update test

--- src/test/java/org/jenkinsci/plugins/redminemetrics/RedmineMetricsCalculatorTest.java
+++ src/test/java/org/jenkinsci/plugins/redminemetrics/RedmineMetricsCalculatorTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import mockit.NonStrictExpectations;
@@ -30,22 +31,33 @@ public class RedmineMetricsCalculatorTest {
         p.setName("Example");
         projects.add(p);
         returns(projects);
+
         redmineManager.getVersions(p.getId());
         ArrayList<Version> versions = new ArrayList<Version>();
         Version v = new Version();
+        v.setId(1);
         v.setName("v1");
         versions.add(v);
         returns(versions);
+
         Map<String, String> params = new HashMap<String, String>();
         params.put("project_id", p.getId().toString());
-        params.put("fixed_version_id", "v1");
+        params.put("fixed_version_id", "1");
         params.put("status_id", "*");
         redmineManager.getIssues(params);
-        returns(new ArrayList<Issue>());
+        List<Issue> issues = new ArrayList<Issue>();
+        Issue issue = new Issue();
+        issue.setStatusId(1);
+        issue.setId(1);
+        issue.setSubject("Hello");
+        issue.setStatusName("Open");
+        issues.add(issue);
+        returns(issues);
       }
     };
+
     RedmineMetricsCalculator rmc = new RedmineMetricsCalculator(
         "http://example.com/", "APIKEY", "Example", "v1");
-    assertEquals(0, rmc.calc().size());
+    assertEquals(1, rmc.calc().size());
   }
 }
