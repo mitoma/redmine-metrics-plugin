0d4b18744dd5dd4d936a91dc9246ce7e8bdfc46d
Mitoma Ryo <mutetheradio@gmail.com>
2012-10-10 23:20:16 +0900

    update graph style.

--- src/main/java/org/jenkinsci/plugins/redminemetrics/MetricsGraph.java
+++ src/main/java/org/jenkinsci/plugins/redminemetrics/MetricsGraph.java
@@ -4,7 +4,9 @@ import hudson.model.AbstractProject;
 import hudson.model.Run;
 import hudson.util.Graph;
 import hudson.util.RunList;
+import hudson.util.ShiftedCategoryAxis;
 
+import java.awt.Color;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collections;
@@ -12,21 +14,19 @@ import java.util.List;
 
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.CategoryAxis;
+import org.jfree.chart.axis.CategoryLabelPositions;
+import org.jfree.chart.axis.NumberAxis;
 import org.jfree.chart.plot.CategoryPlot;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.ui.RectangleInsets;
 
 public class MetricsGraph extends Graph {
 
   private AbstractProject<?, ?> project;
 
-  public MetricsGraph(AbstractProject<?, ?> project) {
-    super(Calendar.getInstance(), 640, 480);
-    this.project = project;
-  }
-
-  @Override
-  protected JFreeChart createGraph() {
+  private DefaultCategoryDataset createDataset() {
     List<MetricsAction> actions = new ArrayList<MetricsAction>();
     RunList<?> builds = project.getBuilds();
     for (Run<?, ?> run : builds) {
@@ -47,13 +47,43 @@ public class MetricsGraph extends Graph {
             + action.getBuild().getNumber());
       }
     }
-
-    JFreeChart areaChart = ChartFactory.createStackedAreaChart("Ticket",
-        "BuildNum", "Count", ds, PlotOrientation.VERTICAL, true, true, false);
-    CategoryPlot plot = (CategoryPlot) areaChart.getPlot();
-    plot.getDomainAxis().setUpperMargin(0);
-    plot.getDomainAxis().setLowerMargin(0);
-    plot.getDomainAxis().setCategoryMargin(0);
-    return areaChart;
+    return ds;
   }
+
+  public MetricsGraph(AbstractProject<?, ?> project) {
+    super(Calendar.getInstance(), 640, 480);
+    this.project = project;
+  }
+
+  @Override
+  protected JFreeChart createGraph() {
+    DefaultCategoryDataset dataset = createDataset();
+
+    JFreeChart chart = ChartFactory.createStackedAreaChart("Ticket",
+        "BuildNum", "Count", dataset, PlotOrientation.VERTICAL, true, true,
+        false);
+    chart.setBackgroundPaint(Color.white);
+
+    CategoryPlot plot = (CategoryPlot) chart.getPlot();
+    plot.setBackgroundPaint(Color.WHITE);
+    plot.setOutlinePaint(null);
+    plot.setForegroundAlpha(0.8f);
+    plot.setRangeGridlinesVisible(true);
+    plot.setRangeGridlinePaint(Color.black);
+
+    CategoryAxis domainAxis = new ShiftedCategoryAxis(null);
+    plot.setDomainAxis(domainAxis);
+    domainAxis.setCategoryLabelPositions(CategoryLabelPositions.UP_90);
+    domainAxis.setLowerMargin(0.0);
+    domainAxis.setUpperMargin(0.0);
+    domainAxis.setCategoryMargin(0.0);
+
+    NumberAxis rangeAxis = (NumberAxis) plot.getRangeAxis();
+    rangeAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
+    rangeAxis.setAutoRange(true);
+
+    plot.setInsets(new RectangleInsets(0, 0, 0, 5.0));
+    return chart;
+  }
+
 }
